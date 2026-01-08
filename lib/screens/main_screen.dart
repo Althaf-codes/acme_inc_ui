2439d2a613d20b92afacd5094ba51ca3a485c4de
Althaf-codes <althafarali8@gmail.com>
2026-01-08 11:54:07 +0530

    Added chat screen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:acme_inc/screens/activity/activity_screen.dart';
+import 'package:acme_inc/screens/chat/chat_screen.dart';
 import 'package:acme_inc/screens/home/home_screen.dart';
 import 'package:acme_inc/utils/constant.dart';
 import 'package:flutter/material.dart';
@@ -13,11 +14,11 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  int _currentIndex = 3;
+  int _currentIndex = 1;
 
   final List<Widget> _screens = [
     HomeScreen(),
-    HomeScreen(),
+    ChatScreen(),
     HomeScreen(),
     ActivityScreen(),
     HomeScreen(),
